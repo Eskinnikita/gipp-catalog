9c2a2d207c58b3567aebfde5cd2cc2a37aeefa54
Eskin Nikita <ladeuxmaster@gmail.com>
2021-06-08 23:46:11 +0300

    Fix base url empty string

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ export default {
     meta: [
       {charset: 'utf-8'},
       {name: 'viewport', content: 'width=device-width, initial-scale=1'},
-      {hid: 'description', name: 'description', content: ''}
+      {hid: 'description', name: 'description', content: ''},
     ],
     link: [
       {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
@@ -64,7 +64,7 @@ export default {
 
   env: {
     production: false,
-    serverUrl: process.env.production ? '' : 'http://gipp-server.std-272.ist.mospolytech.ru',
+    serverUrl: 'http://gipp-server.std-272.ist.mospolytech.ru',
     appName: 'Каталог детских СМИ'
   }
 }
